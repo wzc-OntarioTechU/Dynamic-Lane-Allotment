453fb4bc5d5941404b3d4847a3155f140878d70e
wzc-OntarioTechU <william.chamberlain@ontariotechu.net>
2024-12-01 10:47:28 -0500

    Removed Unnessasary components,
    - Removed COAP.
    - Removed WolfSSL.

--- lane-light-sense-edge/main/main.c
+++ lane-light-sense-edge/main/main.c
@@ -1,34 +1,10 @@
-#include "wifi_helper.h"
-#include "coap_helper.h"
+#include "bt_helper.h"
+#include "esp_01_helper.h"
 #include "led_helper.h"
 
 bool light_strip = false;
 
-// call on the coap receiver
-void coap_receive_task() {
-    while (true) {
-        recieve_message();
-        // some very small delay to give a change for other things to run
-        vTaskDelay(pdMS_TO_TICKS(1));
-    }
-}
-
-void coap_msg_receiver(char* data, size_t length) {
-
-}
-
-void ble_scan_task() {
-    
-}
 
 void app_main() {
-    // init wifi
-    wifi_sta_init();
-    // init coap
-    coap_init();
-    set_receive_func(&coap_msg_receiver);
-    // init led strip
-    led_strip_init();
-
-
+    
 }
